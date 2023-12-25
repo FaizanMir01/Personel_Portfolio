16e58a0aee87e02f0f3e1d1c436557f6be5b7f45
MirFaizan01 <faizanmeer322@gmail.com>
2023-12-25 22:51:46 +0530

    updated javascript

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,26 @@
+
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
-  
+const cursor = document.querySelector(".cursor");
+const body = document.querySelector("body");
+const logo = document.querySelector(".logo")
+
+body.addEventListener("mousemove",(dets)=>{
+  gsap.to(cursor,{
+    x:dets.x,
+    y:dets.y
+  })
+})
+
+let tl = gsap.timeline();
+tl.from("#desktop-nav",{
+  opacity:0,
+  y:-50,
+  duration:1,
+})
+
+
