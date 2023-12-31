3efae19148e92d03c85853864ce7a0c2f402eea4
MirFaizan01 <faizanmeer322@gmail.com>
2023-12-31 15:47:55 +0530

    added animations for main page

--- portfolio.js
+++ portfolio.js
@@ -5,7 +5,8 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
-const cursor = document.querySelector(".cursor");
+function MouseFollower(){
+  const cursor = document.querySelector(".cursor");
 const body = document.querySelector("body");
 const logo = document.querySelector(".logo")
 
@@ -16,11 +17,24 @@ body.addEventListener("mousemove",(dets)=>{
   })
 })
 
-let tl = gsap.timeline();
-tl.from("#desktop-nav",{
+}
+MouseFollower();
+
+
+gsap.registerPlugin(ScrollTrigger);
+const tl = gsap.timeline()
+tl.from("nav",{
+  y:-100,
   opacity:0,
-  y:-50,
   duration:1,
+  stagger:0
 })
-
-
+tl.from(".Anime1",{
+  x:-100,
+  opacity:0,
+  duration:1
+})
+tl.from(".Anime2",{
+  opacity:0,
+  scale:0.5,
+})
